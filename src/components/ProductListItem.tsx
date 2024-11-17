b6d203ee9cbe4e13d845758431f7f8117fb2b755
Dion Ang <dionang11@gmail.com>
2024-11-17 19:36:35 +0800

    routing

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,49 @@
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+import { Colors } from "../constants/Colors";
+import { Product } from "../types";
+import { Link } from "expo-router";
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+export default function ProductListItem({ product }: ProductListItemProps) {
+  return (
+    <Link href={`/menu/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name}</Text>
+        <Text style={styles.price}>{product.price}</Text>
+      </Pressable>
+    </Link>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: {
+    fontWeight: "600",
+    fontSize: 18,
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+  },
+});
