8a8356383c7a89ff7dea12433fac8bf1fe5cf6f5
Dion Ang <dionang11@gmail.com>
2024-11-20 23:04:06 +0800

    cart quantity

--- src/providers/CarProviders.tsx
+++ src/providers/CarProviders.tsx
@@ -1,22 +1,34 @@
 import { CartItem, Product } from "@/types";
 import { createContext, PropsWithChildren, useContext, useState } from "react";
+import { randomUUID } from "expo-crypto";
 
 type CartType = {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
 };
 
 const CartContext = createContext<CartType>({
   items: [],
   addItem: () => {},
+  updateQuantity: () => {},
 });
 
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
 
   const addItem = (product: Product, size: CartItem["size"]) => {
+    const existingItem = items.find(
+      (item) => item.product === product && item.size === size
+    );
+
+    if (existingItem) {
+      updateQuantity(existingItem.id, 1);
+      return;
+    }
+
     const newCartItem: CartItem = {
-      id: "1",
+      id: randomUUID(),
       product,
       product_id: product.id,
       size,
@@ -26,11 +38,24 @@ const CartProvider = ({ children }: PropsWithChildren) => {
     setItems([newCartItem, ...items]);
   };
 
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    const updatedItems = items
+      .map((item) =>
+        item.id !== itemId
+          ? item
+          : { ...item, quantity: item.quantity + amount }
+      )
+      .filter((item) => item.quantity > 0);
+
+    setItems(updatedItems);
+  };
+
   return (
     <CartContext.Provider
       value={{
         items,
         addItem,
+        updateQuantity,
       }}
     >
       {children}
