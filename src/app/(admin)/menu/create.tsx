c95ea120d7a3d4e1041f4d74fb518a414f478901
Dion Ang <dionang11@gmail.com>
2024-11-23 20:37:06 +0800

    create product form

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,11 +1,31 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Image } from "react-native";
 import React, { useState } from "react";
 import Button from "@/components/Button";
+import { defaultPizzaImage } from "@/components/ProductListItem";
+import { Colors } from "@/constants/Colors";
+import * as ImagePicker from "expo-image-picker";
 
 export default function CreateProductScreen() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [error, setError] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   const resetFields = () => {
     setName("");
@@ -42,8 +62,13 @@ export default function CreateProductScreen() {
 
   return (
     <View style={styles.container}>
-      <Text>Image</Text>
-      <Text>Select Image</Text>
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        style={styles.image}
+      />
+      <Text onPress={pickImage} style={styles.textButton}>
+        Select Image
+      </Text>
 
       <Text style={styles.label}>Name</Text>
       <TextInput
@@ -85,5 +110,16 @@ const styles = StyleSheet.create({
   error: {
     color: "red",
     padding: 5,
+  },
+  image: {
+    width: "50%",
+    aspectRatio: 1,
+    alignSelf: "center",
+  },
+  textButton: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: Colors.light.tint,
+    marginVertical: 10,
   },
 });
